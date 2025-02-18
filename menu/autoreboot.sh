0f8d13922a952ce34e7878c87400250c9a663115
tanilink <rayyan.kontak@gmail.com>
2025-02-18 14:05:27 +0700

    Update autoreboot.sh

--- menu/autoreboot.sh
+++ menu/autoreboot.sh
@@ -1,180 +1,124 @@
 #!/bin/bash
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-UWhite='\033[4;37m'       # White
-On_IPurple='\033[0;105m'  #
-On_IRed='\033[0;101m'
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-NC='\e[0m'
 
-# // Export Color & Information
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
+# ==========================================
+# Color Definitions
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+NC='\e[0m'  # No Color
 
-# // Export Banner Status Information
-export EROR="[${RED} EROR ${NC}]"
-export INFO="[${YELLOW} INFO ${NC}]"
-export OKEY="[${GREEN} OKEY ${NC}]"
-export PENDING="[${YELLOW} PENDING ${NC}]"
-export SEND="[${YELLOW} SEND ${NC}]"
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+# Banner Status Information
+EROR="[${RED} ERROR ${NC}]"
+INFO="[${YELLOW} INFO ${NC}]"
+OKEY="[${GREEN} OKEY ${NC}]"
+PENDING="[${YELLOW} PENDING ${NC}]"
+SEND="[${YELLOW} SEND ${NC}]"
+RECEIVE="[${YELLOW} RECEIVE ${NC}]"
 
-# // Export Align
-export BOLD="\e[1m"
-export WARNING="${RED}\e[5m"
-export UNDERLINE="\e[4m"
+# Align Information
+BOLD="\e[1m"
+WARNING="${RED}\e[5m"
+UNDERLINE="\e[4m"
 
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
+# Get public IP
+MYIP=$(wget -qO- icanhazip.com)
+
+# Checking VPS
 echo "Checking VPS"
 clear
+
+# Creating reboot script if not exists
 if [ ! -e /usr/bin/reboot ]; then
-echo '#!/bin/bash' > /usr/bin/reboot
-echo 'tanggal=$(date +"%m-%d-%Y")' >> /usr/bin/reboot
-echo 'waktu=$(date +"%T")' >> /usr/bin/reboot
-echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /usr/bin/reboot
-echo '/sbin/shutdown -r now' >> /usr/bin/reboot
-chmod +x /usr/bin/reboot
+    echo '#!/bin/bash' > /usr/bin/reboot
+    echo 'tanggal=$(date +"%m-%d-%Y")' >> /usr/bin/reboot
+    echo 'waktu=$(date +"%T")' >> /usr/bin/reboot
+    echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /usr/bin/reboot
+    echo '/sbin/shutdown -r now' >> /usr/bin/reboot
+    chmod +x /usr/bin/reboot
 fi
 
+# Menu
 echo -e ""
 echo -e "------------------------------------" | lolcat
 echo -e "             AUTO REBOOT"
 echo -e "------------------------------------" | lolcat
 echo -e ""
 echo -e "    1)  Auto Reboot 30 Minutes"
-echo -e "    2)  Auto Reboot 1 Hours"
+echo -e "    2)  Auto Reboot 1 Hour"
 echo -e "    3)  Auto Reboot 12 Hours"
 echo -e "    4)  Auto Reboot 24 Hours"
-echo -e "    5)  Auto Reboot 1 Weeks"
-echo -e "    6)  Auto Reboot 1 Mount"
+echo -e "    5)  Auto Reboot 1 Week"
+echo -e "    6)  Auto Reboot 1 Month"
 echo -e "    7)  Turn Off Auto Reboot"
 echo -e ""
 echo -e "------------------------------------" | lolcat
 echo -e "    x)   MENU"
 echo -e "------------------------------------" | lolcat
 echo -e ""
-read -p "     Please Input Number  [1-7 or x] :  "  autoreboot
-echo -e ""
+
+# Validate input
+while true; do
+    read -p "     Please Input Number [1-7 or x] :  " autoreboot
+    if [[ "$autoreboot" =~ ^[1-7x]$ ]]; then
+        break
+    else
+        echo -e "${EROR} Please enter a valid number."
+    fi
+done
+
 case $autoreboot in
 1)
-echo "*/30 * * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
-echo "" > /root/log-reboot.txt
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      AutoReboot : On"
-echo -e "      AutoReboot Every : 30 Minutes"
-echo -e ""
-echo -e "======================================"
-service cron reload >/dev/null 2>&1
-service cron restart >/dev/null 2>&1
-;;
+    echo "*/30 * * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot
+    echo "" > /root/log-reboot.txt
+    echo -e "AutoReboot : On\nAutoReboot Every : 30 Minutes"
+    ;;
 2)
-echo "0 * * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
-echo "" > /root/log-reboot.txt
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      AutoReboot : On"
-echo -e "      AutoReboot Every : 1 Hours"
-echo -e ""
-echo -e "======================================"
-service cron reload >/dev/null 2>&1
-service cron restart >/dev/null 2>&1
-;;
+    echo "0 * * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot
+    echo "" > /root/log-reboot.txt
+    echo -e "AutoReboot : On\nAutoReboot Every : 1 Hour"
+    ;;
 3)
-echo "0 */12 * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
-echo "" > /root/log-reboot.txt
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      AutoReboot : On"
-echo -e "      AutoReboot Every : 12 Hours"
-echo -e ""
-echo -e "======================================"
-service cron reload >/dev/null 2>&1
-service cron restart >/dev/null 2>&1
-;;
+    echo "0 */12 * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot
+    echo "" > /root/log-reboot.txt
+    echo -e "AutoReboot : On\nAutoReboot Every : 12 Hours"
+    ;;
 4)
-echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
-echo "" > /root/log-reboot.txt
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      AutoReboot : On"
-echo -e "      AutoReboot Every : 24 Hours"
-echo -e ""
-echo -e "======================================"
-service cron reload >/dev/null 2>&1
-service cron restart >/dev/null 2>&1
-;;
+    echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot
+    echo "" > /root/log-reboot.txt
+    echo -e "AutoReboot : On\nAutoReboot Every : 24 Hours"
+    ;;
 5)
-echo "0 0 */7 * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
-echo "" > /root/log-reboot.txt
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      AutoReboot : On"
-echo -e "      AutoReboot Every : 1 Weeks"
-echo -e ""
-echo -e "======================================"
-service cron reload >/dev/null 2>&1
-service cron restart >/dev/null 2>&1
-;;
+    echo "0 0 */7 * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot
+    echo "" > /root/log-reboot.txt
+    echo -e "AutoReboot : On\nAutoReboot Every : 1 Week"
+    ;;
 6)
-echo "0 0 1 * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
-echo "" > /root/log-reboot.txt
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      AutoReboot : On"
-echo -e "      AutoReboot Every : 1 Mount"
-echo -e ""
-echo -e "======================================"
-service cron reload >/dev/null 2>&1
-service cron restart >/dev/null 2>&1
-;;
+    echo "0 0 1 * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot
+    echo "" > /root/log-reboot.txt
+    echo -e "AutoReboot : On\nAutoReboot Every : 1 Month"
+    ;;
 7)
-rm -fr /etc/cron.d/auto_reboot
-echo "" > /root/log-reboot.txt
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      AutoReboot Turned Off"
-echo -e ""
-echo -e "======================================"
+    rm -f /etc/cron.d/auto_reboot
+    echo "" > /root/log-reboot.txt
+    echo -e "AutoReboot Turned Off"
+    ;;
+x)
+    menu
+    ;;
+*)
+    echo -e "${EROR} Invalid option."
+    ;;
+esac
+
+# Restart cron service to apply changes
 service cron reload >/dev/null 2>&1
 service cron restart >/dev/null 2>&1
-;;
-x)
-menu
-;;
-*)
-echo "Please enter an correct number"
-;;
-esac
-read -n 1 -s -r -p "Press any key to back on menu"
+
+# Pause and go back to menu
+read -n 1 -s -r -p "Press any key to return to menu"
 
 menu
