a61206a0a01b1f81ed70472a9f92e743af7aabc5
tanilink <rayyan.kontak@gmail.com>
2025-02-18 10:13:32 +0700

    Update trial-generator.sh

--- trial/trial-generator.sh
+++ trial/trial-generator.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
 export YELLOW='\033[0;33m'
@@ -6,27 +8,30 @@ export PURPLE='\033[0;35m'
 export CYAN='\033[0;36m'
 export LIGHT='\033[0;37m'
 export NC='\033[0m'
+
 clear
+
 echo -e "${PURPLE}=================================="
 echo -e "${LIGHT}"
 echo -e " Akumulasi Trial minimal 1 menit"
 echo -e ""
-echo -e " 1).TRIAL AKUN SSH & slowdns"
-echo -e " 2).TRIAL AKUN SSH UDP"
-echo -e " 3).TRIAL AKUN VMESS"
-echo -e " 4).TRIAL AKUN VLESS"
-echo -e " 5).TRIAL AKUN TROJAN"
+echo -e " 1). TRIAL AKUN SSH & slowdns"
+echo -e " 2). TRIAL AKUN SSH UDP"
+echo -e " 3). TRIAL AKUN VMESS"
+echo -e " 4). TRIAL AKUN VLESS"
+echo -e " 5). TRIAL AKUN TROJAN"
 echo -e "${PURPLE}==================================="
 
 read -p "PILIH NOMOR: " bro
 echo -e ""
+
 case $bro in
-1) clear ; trial ;;
-2) clear ; trial-udp ;;
-3) clear ; trial-vmess ;;
-4) clear ; trial-vless ;;
-5) clear ; trial-trojan ;;
-100) clear ; $up2u ;;
-00 | 0) clear ; menu ;;
-*) clear ; menu ;;
+    1) trial ;;
+    2) trial-udp ;;
+    3) trial-vmess ;;
+    4) trial-vless ;;
+    5) trial-trojan ;;
+    100) $up2u ;;
+    00 | 0) menu ;;
+    *) menu ;;
 esac
