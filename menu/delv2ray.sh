f4977b071e85fa6694d5bdc8fb3aa153f4b04df2
tanilink <rayyan.kontak@gmail.com>
2025-02-18 14:15:31 +0700

    Update delv2ray.sh

--- menu/delv2ray.sh
+++ menu/delv2ray.sh
@@ -12,55 +12,66 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- ipinfo.io/ip)
 echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "\e[0m${GREEN}Permission Accepted...\e[0m"
+IZIN=$(curl -s ipinfo.io/ip | grep -q $MYIP)
+if [ $? -eq 0 ]; then
+    echo -e "\e[0m${GREEN}Permission Accepted...\e[0m"
 else
-echo -e "\e[0m${RED}Permission Denied!\e[0m";
-echo -e "\e[0m${LIGHT}Fuck You!!"
-exit 0
+    echo -e "\e[0m${RED}Permission Denied!\e[0m"
+    echo -e "\e[0m${LIGHT}Access denied!"
+    exit 0
 fi
-clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo ""
-		echo "You have no existing clients!"
-		exit 1
-	fi
 
-	clear
-	echo ""
+clear
+
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
+if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+    echo ""
+    echo "You have no existing clients!"
+    exit 1
+fi
+
+clear
+echo ""
 echo -e "\e[33;1m┌─────────────────────────────────────────────────┐\e[0m"
-echo -e "\e[33;1m│\e[44;97;1m            • DELETED XRAY USER •             \e[0m"             
+echo -e "\e[33;1m│\e[44;97;1m            • DELETED XRAY USER •             \e[0m"
 echo -e "\e[33;1m└─────────────────────────────────────────────────┘\e[0m"
 echo ""
-	echo "     No  Expired   User"
-	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
-		fi
-	done
+echo "     No  Expired   User"
+grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+
+# Input client selection
+until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+    if [[ ${CLIENT_NUMBER} == '1' ]]; then
+        read -rp "Select one client [1]: " CLIENT_NUMBER
+    else
+        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+    fi
+done
+
+# Get the user and expiration details
 user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+
+# Remove the user from config.json
 sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
 
+# Remove the user's specific config files
 rm -f /etc/xray/vmess-$user-tls.json /etc/xray/vmess-$user-nontls.json
+
+# Restart xray service
 systemctl restart xray.service
+
 clear
 echo ""
 echo -e "\e[33;1m┌─────────────────────────────────────────────────┐\e[0m"
-echo -e "\e[33;1m│\e[44;97;1m           • DELETED XRAY USER •             \e[0m"             
+echo -e "\e[33;1m│\e[44;97;1m           • DELETED XRAY USER •             \e[0m"
 echo -e "\e[33;1m└─────────────────────────────────────────────────┘\e[0m"
 echo -e ""
 echo -e "\e[96;1m Username  : $user  \e[0m"
 echo -e "\e[96;1m Expired   : $exp   \e[0m"
 echo -e ""
 echo -e "\e[33;1m┌─────────────────────────────────────────────────┐\e[0m"
-echo -e "\e[33;1m│\e[44;97;1m                • TANILINK •                 \e[0m"             
+echo -e "\e[33;1m│\e[44;97;1m                • TANILINK •                 \e[0m"
 echo -e "\e[33;1m└─────────────────────────────────────────────────┘\e[0m"
-
